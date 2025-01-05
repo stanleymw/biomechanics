98fdf9720e8a23316696ea2ca60f54b987ac3db6
yavko <yavornkolev@gmail.com>
2025-01-04 18:41:22 -0800

    Add multiple screens

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,9 @@ fn range(len: usize) []i32 {
 const screenWidth = 1920;
 const screenHeight = 1080;
 const fontSize = 64;
+
+const Screen = enum { MainMenu, Globe, Play };
+
 pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "game2");
     defer rl.closeWindow(); // Close window and OpenGL context
@@ -23,7 +26,8 @@ pub fn main() anyerror!void {
         ascii,
     ));
 
-    var showMessageBox = false;
+    //var showMessageBox = false;
+    var currentScreen: Screen = .MainMenu;
 
     // Main game loop
     while (!rl.windowShouldClose()) {
@@ -31,18 +35,23 @@ pub fn main() anyerror!void {
         {
             rl.beginDrawing();
             defer rl.endDrawing();
-
             rl.clearBackground(rl.Color.white);
 
-            rl.drawText("Feelz", 190, 200, 20, rl.Color.light_gray);
-            if (rg.guiButton(rl.Rectangle.init(10, 10, 256, 64), "Sentir") > 0) {
-                showMessageBox = true;
-            }
+            switch (currentScreen) {
+                .MainMenu => {
+                    rl.drawText("Feelz", 190, 200, 20, rl.Color.light_gray);
+                    if (rg.guiButton(rl.Rectangle.init(10, 10, 256, 64), "Sentir") > 0) {
+                        currentScreen = .Globe;
+                    }
 
-            if (showMessageBox) {
-                if (rg.guiMessageBox(rl.Rectangle.init(screenWidth / 2, screenHeight / 2, 512, 256), "#191#Message Box", "Hi! This is a message!", "Nice;Cool") >= 0) {
-                    showMessageBox = false;
-                }
+                    // if (showMessageBox) {
+                    //     if (rg.guiMessageBox(rl.Rectangle.init(screenWidth / 2, screenHeight / 2, 512, 256), "#191#Message Box", "Hi! This is a message!", "Nice;Cool") >= 0) {
+                    //         showMessageBox = false;
+                    //     }
+                    // }
+                },
+                .Globe => {},
+                .Play => {},
             }
         }
     }
