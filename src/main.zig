68af30b339346fc86786b71dea2cb3abdfba683c
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-01-04 18:28:08 -0800

    use loadfontex for better font control

--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,26 @@ const rl = @import("raylib");
 const rg = @import("raygui");
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    const screenWidth = 1280;
-    const screenHeight = 720;
+fn range(len: usize) []i32 {
+    return @as([*]i32, undefined)[0..len];
+}
 
+const screenWidth = 1280;
+const screenHeight = 720;
+const fontSize = 36;
+pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "game2");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(240);
-    rg.guiSetStyle(rg.GuiControl.default, rg.GuiDefaultProperty.text_size, 30);
-    rg.guiSetFont(rl.loadFont("resources/font.otf"));
+    rg.guiSetStyle(rg.GuiControl.default, rg.GuiDefaultProperty.text_size, fontSize);
+
+    const ascii = range(256);
+    rg.guiSetFont(rl.loadFontEx(
+        "resources/font.otf",
+        fontSize,
+        ascii,
+    ));
 
     var showMessageBox = false;
 
