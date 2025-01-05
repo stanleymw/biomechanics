8cc9b8af100a49f273d90922f46d45285cc7891b
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-01-04 18:01:10 -0800

    add computer modern font

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const rl = @import("raylib");
 const rg = @import("raygui");
+const std = @import("std");
+
+fn range(len: usize) []i32 {
+    return @as([*]i32, undefined)[0..len];
+}
 
 pub fn main() anyerror!void {
     const screenWidth = 1280;
@@ -9,6 +14,16 @@ pub fn main() anyerror!void {
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(240);
+    rg.guiSetStyle(rg.GuiControl.default, rg.GuiDefaultProperty.text_size, 30);
+    const feelz = @embedFile("resources/font.otf");
+
+    const lol = range(255);
+    rg.guiSetFont(rl.loadFontFromMemory(
+        ".otf",
+        feelz,
+        30,
+        lol,
+    ));
 
     var showMessageBox = false;
 
@@ -21,13 +36,13 @@ pub fn main() anyerror!void {
 
             rl.clearBackground(rl.Color.white);
 
-            rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
-            if (rg.guiButton(rl.Rectangle.init(10, 10, 64, 16), "Sentir") > 0) {
+            rl.drawText("Feelz", 190, 200, 20, rl.Color.light_gray);
+            if (rg.guiButton(rl.Rectangle.init(10, 10, 128, 48), "Sentir") > 0) {
                 showMessageBox = true;
             }
 
             if (showMessageBox) {
-                if (rg.guiMessageBox(rl.Rectangle.init(85, 70, 250, 100), "#191#Message Box", "Hi! This is a message!", "Nice;Cool") >= 0) {
+                if (rg.guiMessageBox(rl.Rectangle.init(85, 70, 512, 256), "#191#Message Box", "Hi! This is a message!", "Nice;Cool") >= 0) {
                     showMessageBox = false;
                 }
             }
