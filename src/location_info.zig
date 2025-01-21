92d2d06b9d886cd75b6d21c2dbb4aef689e51737
yavko <yavornkolev@gmail.com>
2025-01-20 18:39:06 -0800

    Add *broken* info pages

---
+++ src/location_info.zig
@@ -0,0 +1,27 @@
+pub const locationInfoText = [_][]const u8{
+    // Solar
+    \\ Photovoltaic Cells: These are the fundamental units that convert sunlight into electrical energy through the photovoltaic effect. Typically made from silicon, they are interconnected to form a solar panel.
+    \\ Encapsulation Layers: Protective layers, usually made of ethylene-vinyl acetate (EVA), encase the photovoltaic cells to shield them from moisture and mechanical damage.
+    \\ Glass Cover: A tempered glass layer covers the front of the panel, protecting the cells from environmental factors like hail, wind, and debris while allowing sunlight to pass through.
+    \\ Backsheet: The rear layer of the panel, often made of durable polymer, provides electrical insulation and protection from environmental stressors.
+    \\ Frame: An aluminum frame surrounds the panel, providing structural support and facilitating mounting onto various surfaces.
+    \\ Junction Box: Located on the backside, the junction box houses electrical connections and bypass diodes, enabling safe and efficient current flow.
+    \\ Operation:
+    \\ Energy Capture: When sunlight strikes the photovoltaic cells, photons are absorbed, exciting electrons and creating electron-hole pairs. 
+    \\ Energy Conversion: The movement of these electrons generates a direct current (DC).
+    \\ Energy Utilization: An inverter converts the DC into alternating current (AC), which can then be used to power electrical devices or fed into the power grid.
+    \\ Environmental Contribution:
+    \\ -Solar panels generate electricity without emitting greenhouse gases during operation, significantly reducing reliance on fossil fuels. This leads to a decrease in air pollutants and helps to control climate change.
+    \\
+    \\ Repair:
+    \\ Photovoltaic Cell: 
+    \\ Inspection: Technicians visually inspect the panel for cracks, discoloration, or hot spots, which may indicate a damaged cell.
+    \\ Testing: Electrical tests, such as using a multimeter, assess the performance of the suspected cell to confirm damage.
+    \\ Bypass Diode Check: Examine the bypass diodes in the junction box to ensure they are functioning correctly, as faulty diodes can affect cell performance.
+    \\ Replacement: If a cell is confirmed to be damaged, the panel may need to be replaced entirely, as individual cell replacement is often impractical due to the encapsulation and sealing of the panel.
+    \\ Reinstallation and Testing: After replacement, the panel is reinstalled, and the system is tested to ensure it operates efficiently.
+    ,
+    \\ y
+    ,
+    \\ z
+};
--- src/main.zig
+++ src/main.zig
@@ -8,12 +8,22 @@ const game = @import("game.zig");
 const screenWidth = 1920;
 const screenHeight = 1080;
 
-const Screen = enum { MainMenu, Globe, Play };
+const Screen = union(enum) { MainMenu, Globe, Play: Place, LocationInfo: Location, Info };
 
-fn drawTextureCentered(scaleFactor: f32, rotation: f32, texture: rl.Texture) void {
+const Location = enum(u4) { SolarPanels, CarbonCapture, Nuclear, Desalination };
+const Place = struct {
+    location: Location,
+    level: u3,
+};
+
+fn drawTextureCenteredAtPoint(scaleFactor: f32, rotation: f32, x: f32, y: f32, texture: rl.Texture2D) void {
     const scaledWidth = @as(f32, @floatFromInt(texture.width)) * scaleFactor;
     const scaledHeight = @as(f32, @floatFromInt(texture.height)) * scaleFactor;
-    rl.drawTextureEx(texture, rl.Vector2.init(screenWidth / 2 - scaledWidth / 2, screenHeight / 2 - scaledHeight / 2), rotation, scaleFactor, rl.Color.white);
+    rl.drawTextureEx(texture, rl.Vector2.init(x - scaledWidth / 2, y - scaledHeight / 2), rotation, scaleFactor, rl.Color.white);
+}
+
+fn drawTextureCentered(scaleFactor: f32, rotation: f32, texture: rl.Texture2D) void {
+    drawTextureCenteredAtPoint(scaleFactor, rotation, screenWidth / 2, screenHeight / 2, texture);
 }
 
 var poiPinTex: rl.Texture2D = undefined;
@@ -24,6 +34,8 @@ const PoiPin = struct {
     x: f32,
     y: f32,
     isLocked: bool = true,
+    isCompleted: bool = false,
+    location: Location,
 
     const Self = @This();
     fn calculateClickBounds(size: f32, x: f32, y: f32) rl.Rectangle {
@@ -38,21 +50,115 @@ const PoiPin = struct {
         const tex =
             if (self.isLocked)
             poiPinLockedTex
-        else if (rl.checkCollisionPointRec(mPos, calculateClickBounds(50, newX, newY))) block: {
-            std.debug.print("pressed!: {}", .{pressed});
+        else if (rl.checkCollisionPointRec(mPos, calculateClickBounds(100, newX, newY))) block: {
+            //std.debug.print("pressed!: {}", .{pressed});
             pressed = rl.isMouseButtonPressed(.left);
             break :block poiPinHoverTex;
         } else poiPinTex;
 
-        rl.drawTextureEx(
-            tex,
-            rl.Vector2.init(newX, newY),
-            0.0,
-            4.0,
-            rl.Color.white,
-        );
+        drawTextureCenteredAtPoint(4.0, 0.0, newX, newY, tex);
 
         return pressed;
+    }
+};
+
+pub const ScrollingTextView = struct {
+    bounds: rl.Rectangle,
+    content_height: f32,
+    scroll: rl.Vector2,
+    view: rl.Rectangle,
+    text_color: rl.Color,
+    background_color: rl.Color,
+    text: []const u8,
+    font_size: i32,
+    font: rl.Font,
+
+    pub fn init(x: f32, y: f32, width: f32, height: f32, initial_text: []const u8, font: rl.Font) ScrollingTextView {
+        return ScrollingTextView{
+            .bounds = rl.Rectangle{
+                .x = x,
+                .y = y,
+                .width = width,
+                .height = height,
+            },
+            .content_height = height,
+            .scroll = rl.Vector2.init(0, 0),
+            .view = rl.Rectangle.init(0, 0, 0, 0),
+            .font_size = 30,
+            .text_color = rl.Color.black,
+            .background_color = rl.Color.ray_white,
+            .text = initial_text,
+            .font = font,
+        };
+    }
+
+    pub fn setText(self: *ScrollingTextView, text: []const u8) void {
+        self.text = text;
+        // Calculate content height based on number of lines
+        var line_count: usize = 1;
+        for (text) |char| {
+            if (char == '\n') line_count += 1;
+        }
+        self.content_height = @as(f32, @floatFromInt(line_count)) *
+            @as(f32, @floatFromInt(self.font_size)) * 1.5;
+    }
+
+    pub fn render(self: *ScrollingTextView) void {
+        const content = rl.Rectangle{
+            .x = 0,
+            .y = 0,
+            .width = self.bounds.width - 20,
+            .height = self.content_height,
+        };
+
+        // Convert scroll result to Vector2
+        const scroll_result = rg.guiScrollPanel(self.bounds, null, content, &self.scroll, &self.view);
+        self.scroll = .{
+            .x = @floatFromInt(scroll_result),
+            .y = self.scroll.y,
+        };
+
+        rl.beginScissorMode(
+            @as(i32, @intFromFloat(self.bounds.x)),
+            @as(i32, @intFromFloat(self.bounds.y)),
+            @as(i32, @intFromFloat(self.bounds.width)),
+            @as(i32, @intFromFloat(self.bounds.height)),
+        );
+
+        const y: f32 = self.bounds.y - self.scroll.y;
+        //var line_start: usize = 0;
+        var current_y: f32 = y;
+
+        // Render each line
+
+        var iter = std.mem.splitScalar(u8, self.text, '\n');
+        while (iter.next()) |line| {
+            rl.drawTextEx(
+                self.font,
+                @ptrCast(line),
+                rl.Vector2.init(self.bounds.x + 4, current_y),
+                @as(f32, @floatFromInt(self.font_size)),
+                1.0,
+                self.text_color,
+            );
+            current_y += @as(f32, @floatFromInt(self.font_size)) * 1.5 + 5;
+        }
+        // for (self.text, 0..) |char, i| {
+        //     if (char == '\n' or i == self.text.len - 1) {
+        //         const line_end = if (char == '\n') i else i + 1;
+        //         rl.drawText(
+        //             @ptrCast(self.text[line_start..line_end]),
+        //             @as(i32, @intFromFloat(self.bounds.x + 4)),
+        //             @as(i32, @intFromFloat(current_y)),
+        //             self.font_size,
+        //             self.text_color,
+        //         );
+        //         line_start = i + 1;
+        //         current_y += @as(f32, @floatFromInt(self.font_size)) * 1.5 + 10;
+        //     }
+        // }
+
+        rl.endScissorMode();
     }
 };
 
@@ -79,10 +185,20 @@ pub fn main() anyerror!void {
     poiPinHoverTex = rl.loadTextureFromImage(rl.loadImage("resources/poi.png"));
 
     var mousePos = rl.Vector2.init(0, 0);
-    const pois = [_]PoiPin{
-        .{ .x = 0.5, .y = 0.5, .isLocked = false },
-        .{ .x = 0.75, .y = 0.2 },
+    var pois = [_]PoiPin{
+        .{ .location = .SolarPanels, .x = 0.5, .y = 0.5, .isLocked = false },
+        .{ .location = .Nuclear, .x = 0.75, .y = 0.2 },
     };
+
+    const info_anchor = rl.Vector2.init(190, 200);
+    var text_view = ScrollingTextView.init(
+        info_anchor.x,
+        info_anchor.y + 50,
+        750,
+        500,
+        "",
+        mainFont,
+    );
 
     // Main game loop
     while (!rl.windowShouldClose()) {
@@ -98,21 +214,58 @@ pub fn main() anyerror!void {
                     rl.drawTextEx(mainFont, "Game", rl.Vector2.init(190, 200), fonts.Size.Medium, 0, rl.Color.light_gray);
 
                     if (rg.guiButton(rl.Rectangle.init(10, 10, 256, 64), "Play") > 0) {
-                        currentScreen = .Globe;
+                        currentScreen = .Info;
                     }
                 },
                 .Globe => {
                     drawTextureCentered(8.0, 0, globeTexture);
-                    var none_pressed = true;
-                    for (pois) |pin| {
-                        none_pressed = none_pressed and !@constCast(&pin).render(mousePos);
+                    var location: ?Location = null;
+                    for (0..pois.len) |idx| {
+                        if (pois[idx].render(mousePos)) {
+                            location = pois[idx].location;
+                            pois[idx].isCompleted = true;
+                        }
                     }
-                    if (!none_pressed) {
-                        currentScreen = .Play;
+                    if (location != null) {
+                        currentScreen = Screen{ .LocationInfo = location.? };
                     }
                 },
                 .Play => {
                     game.render();
+                },
+                .Info => {
+                    currentScreen = .Globe;
+                },
+                .LocationInfo => |*location| {
+                    const info = @import("location_info.zig");
+                    const text = info.locationInfoText[@as(u4, @intFromEnum(location.*))];
+                    const anchor = rl.Vector2.init(190, 200);
+                    rl.drawTextEx(
+                        mainFont,
+                        switch (location.*) {
+                            .SolarPanels => "Solar Panel",
+                            .Nuclear => "Nuclear",
+                            else => "meow",
+                        },
+                        anchor,
+                        fonts.Size.Medium,
+                        0,
+                        rl.Color.light_gray,
+                    );
+                    text_view.setText(text);
+                    text_view.render();
+                    if (rg.guiButton(
+                        rl.Rectangle.init(info_anchor.x, info_anchor.y + 600, 500, 100),
+                        "continue",
+                    ) == 1) {
+                        currentScreen = Screen{
+                            .Play = Place{
+                                .level = 0,
+                                .location = location.*,
+                            },
+                        };
+                    }
+                    // RENDER IMAGE OF MACHINE
                 },
             }
 
